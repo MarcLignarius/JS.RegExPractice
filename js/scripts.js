3079e6003df799ae878cb3891dd90d8e1a0b5fc2
Marc Davies <marcdaviesriot@gmail.com>
2019-12-30 21:16:53 -0800

    add validation script containing regex

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-$(function() {
-
+$(function validatePassword() {
+    var password = document.getElementById('password').value;
+    var passwordREGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@&$]).{6,12}$/;
+    var passwordResult = phoneREGEX.test(phoneNumber);
+  alert("password:"+passwordResult );
 });
