69e22a4658528391458da4266351617c1f372acc
Marc Davies <marcdaviesriot@gmail.com>
2019-12-31 16:38:20 -0800

    add paths depending if password is valid or not

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 $(function() {
-    $("#passwordValidation").submit(function(){
+    $("#passwordValidation").submit(function(event){
+        event.preventDefault();
         var password = document.getElementById('password').value;
         var passwordREGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,12}$/;
         var passwordResult = passwordREGEX.test(password);
-        alert("password:" + passwordResult );
+        // alert("password:" + passwordResult );
+        if (passwordResult === false) {
+            $("#passwordOutput").text("Password does not match all criteria. Please enter a new password.")
+        } else {
+            $("#passwordOutput").text("Password is valid.")
+        }
     });
 });
