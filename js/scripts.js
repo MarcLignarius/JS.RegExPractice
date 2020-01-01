4b2b9fde4f67e4b8a39a2586a7b0845ec6cc0686
Marc Davies <marcdaviesriot@gmail.com>
2019-12-31 16:45:27 -0800

    remove commented code

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,8 @@ $(function() {
         var password = document.getElementById('password').value;
         var passwordREGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,12}$/;
         var passwordResult = passwordREGEX.test(password);
-        // alert("password:" + passwordResult );
         if (passwordResult === false) {
-            $("#passwordOutput").text("Password does not match all criteria. Please enter a new password.")
+            $("#passwordOutput").text("Password is invalid. Please enter a new password.")
         } else {
             $("#passwordOutput").text("Password is valid.")
         }
