c34bb7364e32ec219c57631e0a8bdbcab4b9a379
Marc Davies <marcdaviesriot@gmail.com>
2019-12-31 16:32:25 -0800

    fix indentation of html and rewrite function so that it only triggers on submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-$(function validatePassword() {
-    var password = document.getElementById('password').value;
-    var passwordREGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@&$]).{6,12}$/;
-    var passwordResult = passwordREGEX.test(password);
-  alert("password:"+passwordResult );
+$(function() {
+    $("#passwordValidation").submit(function(){
+        var password = document.getElementById('password').value;
+        var passwordREGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,12}$/;
+        var passwordResult = passwordREGEX.test(password);
+        alert("password:" + passwordResult );
+    });
 });
